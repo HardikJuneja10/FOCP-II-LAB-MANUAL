d1275a001e3269b7ac08177906112b1fb8447499
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-01-19 21:23:45 +0530

    Add files via upload

---
+++ class3.cpp
@@ -0,0 +1,11 @@
+// without using namespace std
+#include<iostream>
+//using namespace std;
+int main(){
+
+    int a=10;
+    float b=10.5;
+    char c='a';
+
+    std::cout<<"THE VALUE OF A is " <<a<<std::endl <<"THE VALUE OF B is "<<b <<std::endl<<"THE VALUE OF C IS "<<c;
+}
