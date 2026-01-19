02d97c65a810e156ba9e84e368da98b070a34cf5
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-01-19 14:06:25 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+//hello world
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a= 10;
+    float b=20.5;
+
+    cout<<"THE VALUE OF A IS "<<a<<endl;
+    cout<<"THE VALUE OF B IS "<<b;
+
+    return 0;
+}
