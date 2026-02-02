3f371d14d72e450cadd879b95f878a42aeb6049c
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-02-02 13:23:35 +0530

    Add files via upload

---
+++ ques11labmanual.cpp
@@ -0,0 +1,21 @@
+// Q11. A wholesale supplier applies 10% discount if the order > 1000 items. Implement a solution to compute total
+// expenses and apply a discount accordingly.
+
+#include<iostream>
+// using namespace std;
+int main(){
+    int c,n;
+
+    std::cout<<"ENTER THE NUMBER OF ENTITIES";
+    std::cin>>n;
+
+    std::cout<<"ENTER THE COST OF ONE ENTITY";
+    std::cin>>c;
+
+    if(n>1000){
+        std::cout<<"THE TOTAL BILL IS "<<(n*c)-(0.1*n*c);  
+    }
+    else
+    std::cout<<"THE TOTAL BILL IS "<<n*c;
+
+}
