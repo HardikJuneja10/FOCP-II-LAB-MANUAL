a6608aca4c0a4a567d8736609d1ef44a82892ffe
HardikJuneja10 <hardikjuneja10@gmail.com>
2026-03-24 22:47:15 +0530

    Add files via upload

---
+++ while4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n;
+
+    cout<<"ENTER N ";
+    cin>>n;
+
+
+    i=1;
+    while(i<=n){
+       // i++; //
+        cout<<i<<" ";
+        i++;
+    }
+
+
+}
+
---
+++ whileodd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+
+    i=1;
+    while(i<=50){
+        if(i%2!=0)
+        cout<<i<<" ";
+        i++;
+    }
+}
